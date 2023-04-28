f90e1df4756d08c9f901da184fca0b94bc880cf0
hexu <hexu_1985@sina.com>
2023-04-28 10:55:28 +0800

    add calculate_motion_trail/draw_rectangle

---
+++ project/calculate_motion_trail/draw_rectangle/exercise1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+rect=plt.Rectangle(
+        (1, 2),  # (x,y)矩形左下角
+        3,  # width长
+        4,  # height宽
+        color='maroon', 
+        alpha=0.5      )
+ax1.add_patch(rect)
+plt.xlim(-1, 6)
+plt.ylim(1, 7)
+plt.show()
+
